1e585fafa4f71d9048f05e30b09206b27d640c3f
vinothsmart <s.vinothkanna90@gmail.com>
2022-10-19 16:27:32 +0530

    contoller added

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,20 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.status(200).json({ message: "Get goals" });
-});
+const {
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+} = require("../controllers/goalController");
 
-router.post("/", (req, res) => {
-  res.status(200).json({ message: "Set Goal" });
-});
+router.get("/", getGoals);
 
-router.put("/:id", (req, res) => {
-  res.status(200).json({ message: `Update Goal ${req.params.id}` });
-});
+router.post("/", setGoal);
 
-router.delete("/:id", (req, res) => {
-  res.status(200).json({ message: `Delete Goal ${req.params.id}` });
-});
+router.put("/:id", updateGoal);
+
+router.delete("/:id", deleteGoal);
 
 module.exports = router;
