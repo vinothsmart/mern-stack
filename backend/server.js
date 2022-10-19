56e40072788d82f4e0c13651feccf82e18f7207b
vinothsmart <s.vinothkanna90@gmail.com>
2022-10-19 15:25:51 +0530

    api updated

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log("Hello world");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("api/goals", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
