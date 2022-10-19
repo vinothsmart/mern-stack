3902d216a5d690c835226b96d5328c350d0038cf
vinothsmart <s.vinothkanna90@gmail.com>
2022-10-19 17:41:59 +0530

    crud api for goals

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -23,21 +23,38 @@ const setGoal = asyncHandler(async (req, res) => {
     text: req.body.text,
   });
 
-  res.status(200).json({ message: "Set goal" });
+  res.status(200).json(goal);
 });
 
 // @desc Update Goal
 // @route PUT /api/goals/:id
 // @access Private
 const updateGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Update Goal ${req.params.id}` });
+  const goal = await Goal.findById(req.params.id);
+
+  if (!goal) {
+    res.status(404);
+    throw new Error("Goal not found");
+  }
+
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  res.status(200).json(updatedGoal);
 });
 
 // @desc Delete Goal
 // @route DELETE /api/goals/:id
 // @access Private
 const deleteGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Delete Goal ${req.params.id}` });
+  const goal = await Goal.findById(req.params.id);
+  if (!goal) {
+    res.status(404);
+    throw new Error("Goal not found");
+  }
+  await goal.remove();
+  res.status(200).json({ id: req.params.id });
 });
 
 module.exports = {
