b22752643b15166b96db905d33efcf110dd8f426
vinothsmart <s.vinothkanna90@gmail.com>
2022-11-22 17:03:45 +0530

    in schema user details added

--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -2,6 +2,11 @@ const moongoose = require("mongoose");
 
 const goalSchema = moongoose.Schema(
   {
+    user: {
+      type: moongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
     text: {
       type: String,
       required: [true, "Please add a text field"],
