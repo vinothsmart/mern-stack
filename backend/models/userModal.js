3128cce56f74c66fd43ca86ee183285d004c059e
vinothsmart <s.vinothkanna90@gmail.com>
2022-11-22 17:02:12 +0530

    userschema created

---
+++ backend/models/userModal.js
@@ -0,0 +1,28 @@
+const moongoose = require("mongoose");
+
+const userSchema = moongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add a name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please add an email"],
+      unique: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please add a valid email",
+      ],
+    },
+    password: {
+      type: String,
+      required: [true, "Please add a password"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = moongoose.model("User", userSchema);
