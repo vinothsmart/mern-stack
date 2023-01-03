c5ebe4797465163866693741f3ca8e98ebbf5ab2
vinothsmart <s.vinothkanna90@gmail.com>
2023-01-03 13:07:50 +0530

    getting error

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,23 +1,33 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+
 // @desc Register a new user
 // @route POST /api/users
 // @access Public
-const registerUser = (req, res) => {
-  res.json({ message: "Register User" });
-};
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please fill all fields");
+  }
+  res.json({ message: "Register111111111 User" });
+});
 
 // @desc Authenticate user
 // @route POST /api/users/login
 // @access Public
-const loginUser = (req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
   res.json({ message: "login User" });
-};
+});
 
 // @desc Get user data
 // @route GET /api/users/me
 // @access Public
-const getUserData = (req, res) => {
+const getUserData = asyncHandler(async (req, res) => {
   res.json({ message: "Get User Data" });
-};
+});
 
 module.exports = {
   registerUser,
