813cb034ce4d2660b6bd60b1f5329a2e44a143b4
vinothsmart <s.vinothkanna90@gmail.com>
2023-01-12 14:43:27 +0530

    authmiddelware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -66,7 +66,7 @@ const loginUser = asyncHandler(async (req, res) => {
 
 // @desc Get user data
 // @route GET /api/users/me
-// @access Public
+// @access Private
 const getUserData = asyncHandler(async (req, res) => {
   res.json({ message: "Get User Data" });
 });
