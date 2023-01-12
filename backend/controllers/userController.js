8e0a95fd7e643a49b015a15466e6419f7647ca2c
vinothsmart <s.vinothkanna90@gmail.com>
2023-01-12 14:33:14 +0530

    jwt token created for login and register

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -36,6 +36,7 @@ const registerUser = asyncHandler(async (req, res) => {
       _id: user._id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
@@ -55,6 +56,7 @@ const loginUser = asyncHandler(async (req, res) => {
       _id: user._id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
@@ -68,6 +70,13 @@ const loginUser = asyncHandler(async (req, res) => {
 const getUserData = asyncHandler(async (req, res) => {
   res.json({ message: "Get User Data" });
 });
+
+// Generate token
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
 
 module.exports = {
   registerUser,
