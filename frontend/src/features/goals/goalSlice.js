1e9facad52f6afcabee3344779b0b14eefee27ac
vinothsmart <s.vinothkanna90@gmail.com>
2023-02-08 15:03:47 +0530

    loading issue sloved I think so

--- frontend/src/features/goals/goalSlice.js
+++ frontend/src/features/goals/goalSlice.js
@@ -29,18 +29,23 @@ export const createGoal = createAsyncThunk(
 );
 
 // Get user goals
-export const getGoals = createAsyncThunk("goals/getAll", async (thunkAPI) => {
-  try {
-    const token = thunkAPI.getState().auth.user.token;
-    return await goalService.getGoals(token);
-  } catch (error) {
-    const message =
-      (error.response && error.response.data && error.response.data.message) ||
-      error.message ||
-      error.toString();
-    return thunkAPI.rejectWithValue(message);
+export const getGoals = createAsyncThunk(
+  "goals/getAll",
+  async (_, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token;
+      return await goalService.getGoals(token);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
   }
-});
+);
 
 export const goalSlice = createSlice({
   name: "goal",
